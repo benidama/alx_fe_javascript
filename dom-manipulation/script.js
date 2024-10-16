84ab14c890475d2e4a71d03d177c5936d950e4ac
benidama <benidama02@gmail.com>
2024-10-16 15:49:23 +0000

    adding math.random method

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,4 +4,7 @@ function showRandomQuote() {
   const random = (document.getElementById("quoteDisplay").innerHTML = array);
   return random;
 }
-function createAddQuoteForm() {}
+function createAddQuoteForm() {
+  const application = Math.random(random);
+  return application;
+}
