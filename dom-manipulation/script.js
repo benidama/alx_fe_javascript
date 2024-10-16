a0da479bd85e91f217cb21c1d9b64de083b78f4d
benidama <benidama02@gmail.com>
2024-10-16 16:22:47 +0000

    Dynamic Quote Addition

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -2,9 +2,13 @@ const array = ["text", "category"];
 
 function showRandomQuote() {
   const random = (document.getElementById("quoteDisplay").innerHTML = array);
-  return random;
-}
-function createAddQuoteForm() {
   const application = Math.random(random);
   return application;
+}
+function createAddQuoteForm() {
+  const divEl = document.createElement("div");
+  const append = body.appendChild(divEl);
+  const inputEl = document.createElements("input");
+  const child = append.appendChild(inputEl);
+  return child;
 }
