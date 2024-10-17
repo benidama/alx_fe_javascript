e50b2dcc673b6765246c41d89ccad34730d3da93
benidama <benidama02@gmail.com>
2024-10-17 20:38:58 +0000

    Implement Json

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -18,3 +18,14 @@ function createAddQuoteForm() {
 function addQuote() {
   return createAddQuoteForm();
 }
+
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function (event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    saveQuotes();
+    alert("Quotes imported successfully!");
+  };
+  fileReader.readAsText(event.target.files[0]);
+}
